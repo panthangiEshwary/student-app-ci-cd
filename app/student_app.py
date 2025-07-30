2d784cc41f0ff90f8d887800499ed58448043140
Panthangi Eshwary <panthangi@gmail.com>
2025-07-30 12:38:44 +0200

    port updated in student app.py file

--- app/student_app.py
+++ app/student_app.py
@@ -9,7 +9,8 @@ db_config = {
     'host': os.environ.get("DB_HOST"),
     'user': os.environ.get("DB_USER"),
     'password': os.environ.get("DB_PASSWORD"),
-    'database': os.environ.get("DB_NAME")
+    'database': os.environ.get("DB_NAME"),
+    'port': int(os.environ.get("DB_PORT", 3306))
 }
 
 @app.route("/")
