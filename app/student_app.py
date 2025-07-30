29ce5c6595dc127ce9b5d1a646c96f1cdaf306d5
Panthangi Eshwary <panthangi@gmail.com>
2025-07-30 12:41:15 +0200

    code changed

--- app/student_app.py
+++ app/student_app.py
@@ -4,7 +4,6 @@ import os
 
 app = Flask(__name__)
 
-# MySQL connection from environment variables
 db_config = {
     'host': os.environ.get("DB_HOST"),
     'user': os.environ.get("DB_USER"),
@@ -23,23 +22,31 @@ def student_page():
 
 @app.route("/add_student", methods=["POST"])
 def add_student():
-    data = request.get_json()
-    conn = mysql.connector.connect(**db_config)
-    cur = conn.cursor()
-    cur.execute("INSERT INTO students (name, email, course) VALUES (%s, %s, %s)",
-                (data['name'], data['email'], data['course']))
-    conn.commit()
-    conn.close()
-    return jsonify({"message": "Student added"}), 201
+    try:
+        data = request.get_json()
+        conn = mysql.connector.connect(**db_config)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO students (name, email, course) VALUES (%s, %s, %s)",
+                    (data['name'], data['email'], data['course']))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return jsonify({"message": "Student added"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route("/students", methods=["GET"])
 def get_students():
-    conn = mysql.connector.connect(**db_config)
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM students")
-    rows = cur.fetchall()
-    conn.close()
-    return jsonify([{"id": r[0], "name": r[1], "email": r[2], "course": r[3]} for r in rows])
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM students")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        return jsonify([{"id": r[0], "name": r[1], "email": r[2], "course": r[3]} for r in rows])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route("/health", methods=["GET"])
 def health():
